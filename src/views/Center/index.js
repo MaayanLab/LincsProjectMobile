ab2db4424a8f33de9ae2dfa157ea1378dffa4319
Edward He <edward.he@macaulay.cuny.edu>
2016-11-22 12:40:50 -0500

    Convert CenterView from class Component to pure function.

--- src/views/Center/index.js
+++ src/views/Center/index.js
@@ -1,48 +1,32 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import {
   View,
   Text,
   Image,
 } from 'react-native';
 
-import Home from '../Home';
 import Unavailable from '../Unavailable';
 
 import AppStyles from '../../styles';
 import styles from './CenterStyleSheet';
 
-export default class Center extends Component {
-  static propTypes = {
-    navigator: PropTypes.object.isRequired,
-    center: PropTypes.string,
-    description: PropTypes.string,
-    // logo: PropTypes.object,
-  }
+export default function Center(props) {
+  const { center, logo, description } = props;
+  if (center.length === 0 || description.length === 0) return <Unavailable />;
 
-  constructor(props) {
-    super(props);
-  }
-
-  _navigate = (navbarTitle) => {
-    this.props.navigator.push({
-      title: navbarTitle,
-      component: Home,
-      index: 2,
-    });
-  }
-
-  render = () => {
-    const { center, logo, description } = this.props;
-    if (center.length === 0 || description.length === 0) return <Unavailable />;
-
-    return (
-      <View style={[AppStyles.container, AppStyles.paddingHorizontal, AppStyles.containerCentered]}>
-        <Image style={[styles.centerLogo]} source={logo} />
-        <Text style={[AppStyles.baseText]}>
-          {description}
-        </Text>
-        <View style={[AppStyles.spacer_10]} />
-      </View>
-    );
-  }
+  return (
+    <View style={[AppStyles.container, AppStyles.paddingHorizontal, AppStyles.containerCentered]}>
+      <Image style={[styles.centerLogo]} source={logo} />
+      <Text style={[AppStyles.baseText]}>
+        {description}
+      </Text>
+      <View style={[AppStyles.spacer_10]} />
+    </View>
+  );
 }
+
+Center.propTypes = {
+  center: PropTypes.string,
+  description: PropTypes.string,
+  // logo: PropTypes.object,
+};
