a8e0f0f6d5b6052c19a11983318d8827e063520b
Edward He <edward.he@macaulay.cuny.edu>
2016-12-05 17:17:34 -0500

    Clean up.

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,4 @@
 import { AppRegistry } from 'react-native';
-// import LincsProjectMobile from './src/LincsProjectMobile.js';
-//
-// AppRegistry.registerComponent('LincsProjectMobile', () => LincsProjectMobile);
-
 import AppContainer from './src/containers';
 
 AppRegistry.registerComponent('LincsProjectMobile', () => AppContainer);
