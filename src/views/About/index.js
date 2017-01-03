23dafd216b506c4e1b1293d721ca413473b78def
Edward He <edward.he@macaulay.cuny.edu>
2017-01-03 16:13:29 -0500

    Resolve linter issues.

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -42,21 +42,22 @@ export default class About extends Component {
         <Text style={[AppStyles.h3, AppStyles.centered]}> Overview </Text>
         <View style={AppStyles.spacer_10} />
         <Text style={[AppStyles.baseText, AppStyles.p, styles.intro]}>
-          &nbsp;&nbsp;&nbsp;&nbsp;By generating and making public data that indicates how cells respond
-          to various genetic and environmental stressors, the LINCS project
-          will help us gain a more detailed understanding of cell pathways
-          and aid efforts to develop therapies that might restore perturbed
-          pathways and networks to their normal states.
+          &nbsp;&nbsp;&nbsp;&nbsp;By generating and making public data that
+          indicates how cells respond to various genetic and environmental
+          stressors, the LINCS project will help us gain a more detailed
+          understanding of cell pathways and aid efforts to develop
+          therapies that might restore perturbed pathways and networks
+          to their normal states.
         </Text>
         <View style={AppStyles.spacer_10} />
         <Text style={[AppStyles.baseText, AppStyles.p, styles.intro]}>
-          &nbsp;&nbsp;&nbsp;&nbsp;The LINCS website is
-          a source of information for the research community and general public
-          about the LINCS project. This website along with the LINCS Data Portal
-          contains details about the assays, cell types, and perturbagens that
-          are currently part of the library, as well as links to participating
-          sites, data releases from the sites, and software that can be used
-          for analyzing the data.
+          &nbsp;&nbsp;&nbsp;&nbsp;The LINCS website is a source of information
+          for the research community and general public about the LINCS project.
+          This website along with the LINCS Data Portal contains details
+          about the assays, cell types, and perturbagens that are currently
+          part of the library, as well as links to participating sites,
+          data releases from the sites, and software that can be used for
+          analyzing the data.
         </Text>
         <View style={AppStyles.spacer_20} />
         <Button style={[styles.button]} onPress={() => this.navigate('LINCS Project')}>
